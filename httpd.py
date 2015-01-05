6b4407a52d6d3d8e115606d288cb145276e4d6e9
Cheng Ming <ming.cheng@shenma-inc.com>
2015-01-05 14:47:47 +0800

    Added hacker codes to shield codes in HTTPRequestHandler that slow down the response.

--- httpd.py
+++ httpd.py
@@ -8,7 +8,15 @@ Create time: 2015/1/4 14:06
 
 from BaseHTTPServer import HTTPServer
 from CGIHTTPServer import CGIHTTPRequestHandler
-
+import BaseHTTPServer
+ 
+def _bare_address_string(self):
+    host, port = self.client_address[:2]
+    return str(host)
+ 
+BaseHTTPServer.BaseHTTPRequestHandler.address_string = \
+        _bare_address_string
+     
 port = 8080
 httpd = HTTPServer(('', port), CGIHTTPRequestHandler)
 print('Starting simple_httpd on port: ' + str(port))
